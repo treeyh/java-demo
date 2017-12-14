906088057775ea276ddba89a0e9cd1c0d74c3385
yuhai18 <yuhai18@wanda.cn>
2017-12-14 13:40:55 +0800

    spring boot sharding mybatis 支持分库分表

---
+++ spring-boot/spring-boot-sharding-jdbc/src/main/resources/config/mybatis/springboot/shardingjdbc/demo0_create_table.sql
@@ -0,0 +1,76 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50719
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : demo0
+
+ Target Server Type    : MySQL
+ Target Server Version : 50719
+ File Encoding         : 65001
+
+ Date: 14/12/2017 13:39:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_order_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_0`;
+CREATE TABLE `t_order_0` (
+  `order_id` int(11) NOT NULL,
+  `order_no` varchar(32) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `amount` bigint(20) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `last_update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for t_order_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_1`;
+CREATE TABLE `t_order_1` (
+  `order_id` int(11) NOT NULL,
+  `order_no` varchar(32) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `amount` bigint(20) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `last_update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for t_user_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_0`;
+CREATE TABLE `t_user_0` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `last_update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for t_user_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_1`;
+CREATE TABLE `t_user_1` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `last_update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+SET FOREIGN_KEY_CHECKS = 1;
