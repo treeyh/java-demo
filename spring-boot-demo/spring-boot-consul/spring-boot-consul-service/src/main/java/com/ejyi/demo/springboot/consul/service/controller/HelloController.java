e7af7df8bd295fffeeece714061b6ccf4b0712c7
yuhai18 <yuhai18@wanda.cn>
2017-12-19 19:41:50 +0800

    spring-boot-consul-service update

--- spring-boot-demo/spring-boot-consul/spring-boot-consul-service/src/main/java/com/ejyi/demo/springboot/consul/service/controller/HelloController.java
+++ spring-boot-demo/spring-boot-consul/spring-boot-consul-service/src/main/java/com/ejyi/demo/springboot/consul/service/controller/HelloController.java
@@ -2,10 +2,14 @@ package com.ejyi.demo.springboot.consul.service.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/service")
@@ -23,6 +27,30 @@ public class HelloController {
     @RequestMapping("/hi")
     public String home() {
         return "hello ConsulServer ";
+    }
+
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add(@RequestParam Integer a, @RequestParam Integer b){
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a+b;
+        String s = "/add, host:"+instance.getHost() + ", service_id:" + instance.getServiceId() + "; result : "+r+"; port:" + port;
+        logger.info(s);
+        return s;
+    }
+
+    @RequestMapping(value = "/del/{serviceName}", method = RequestMethod.GET)
+    public List<ServiceInstance> del(@PathVariable String serviceName){
+
+        List<ServiceInstance> ls = client.getInstances(serviceName);
+
+        return ls;
     }
 }
 
