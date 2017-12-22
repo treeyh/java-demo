e51c077bf17567c8e2919aadb1a5c062f590964c
yuhai18 <yuhai18@wanda.cn>
2017-12-22 10:39:45 +0800

    spring-boot-consul-service

--- spring-boot-demo/spring-boot-consul/spring-boot-consul-service/src/main/java/com/ejyi/demo/springboot/consul/service/controller/HelloController.java
+++ spring-boot-demo/spring-boot-consul/spring-boot-consul-service/src/main/java/com/ejyi/demo/springboot/consul/service/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +37,10 @@ public class HelloController {
     @Value("${server.port}")
     private Integer port;
 
+
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add(@RequestParam Integer a, @RequestParam Integer b){
         ServiceInstance instance = client.getLocalServiceInstance();
@@ -45,12 +50,18 @@ public class HelloController {
         return s;
     }
 
-    @RequestMapping(value = "/del/{serviceName}", method = RequestMethod.GET)
-    public List<ServiceInstance> del(@PathVariable String serviceName){
+    @RequestMapping(value = "/get/{serviceName}", method = RequestMethod.GET)
+    public List<ServiceInstance> get(@PathVariable String serviceName){
 
         List<ServiceInstance> ls = client.getInstances(serviceName);
 
         return ls;
+    }
+
+    @RequestMapping(value = "/geturl/{serviceName}", method = RequestMethod.GET)
+    public String getUrl(@PathVariable String serviceName){
+
+        return loadBalancer.choose("consul-server").getUri().toString();
     }
 }
 
