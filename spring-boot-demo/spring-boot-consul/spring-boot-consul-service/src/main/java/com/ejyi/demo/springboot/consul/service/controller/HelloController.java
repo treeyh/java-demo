4e4144f9304368b7484bcc172aa37d9f9f122593
yuhai18 <yuhai18@wanda.cn>
2018-01-05 11:18:31 +0800

    新增接口供测试

--- spring-boot-demo/spring-boot-consul/spring-boot-consul-service/src/main/java/com/ejyi/demo/springboot/consul/service/controller/HelloController.java
+++ spring-boot-demo/spring-boot-consul/spring-boot-consul-service/src/main/java/com/ejyi/demo/springboot/consul/service/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.ejyi.demo.springboot.consul.service.controller;
 
+import org.apache.commons.lang.math.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping(value="/service")
@@ -63,5 +64,41 @@ public class HelloController {
 
         return loadBalancer.choose("consul-server").getUri().toString();
     }
+
+    @RequestMapping(value = "/checkUser", method = RequestMethod.GET)
+    public Map<String, Object> checkUser(@RequestParam Integer val){
+        Map<String,Object> map = new HashMap<>();
+
+        if(val > 1000){
+            map.put("code", 403);
+            map.put("msg", "check user error");
+        }else{
+            map.put("code", 200);
+            map.put("msg", "ok");
+        }
+        return map;
+    }
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
+    public Map<String, Object> getUser(@RequestParam Integer id, @RequestParam String name, @RequestParam Integer age){
+        Map<String,Object> map = new HashMap<>();
+
+        Map<String, Object> map2 = new HashMap<>();
+
+        map2.put("id", id);
+        map2.put("name", name);
+        map2.put("age", age);
+        map2.put("score", RandomUtils.nextInt(1000));
+        map2.put("nowTime", new Date());
+
+        map.put("code", 200);
+        map.put("msg", "ok");
+        map.put("date", map2);
+
+        return map;
+    }
+
+
+
 }
 
