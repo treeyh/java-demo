3350d4dd57de121b80c442b07dafe5c132786394
余海 <tree.yu@dadaabc.com>
2018-12-12 14:14:57 +0800

    增加spring-boot-server项目单元测试

--- spring-boot/spring-boot-server/spring-boot-web/src/test/java/com/ejyi/demo/springboot/server/web/mock/DemoControllerTestByMock2.java
+++ spring-boot/spring-boot-server/spring-boot-web/src/test/java/com/ejyi/demo/springboot/server/web/mock/DemoControllerTestByMock2.java
@@ -6,14 +6,19 @@ import com.ejyi.demo.springboot.server.model.result.CallResult;
 import com.ejyi.demo.springboot.server.service.DemoService;
 import com.ejyi.demo.springboot.server.utils.JsonUtils;
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 /**
  * @author 余海
@@ -26,16 +31,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @AutoConfigureMockMvc
 public class DemoControllerTestByMock2 {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @SpyBean
     private DemoService demoService;
 
-    @Test
-    public void testQuery() throws Exception {
-
+    @Before
+    public void befor() {
         IpModel ipModel = new IpModel();
         ipModel.setCity("testCity");
         ipModel.setCountry("testCountry");
         BDDMockito.given(this.demoService.queryByIp("8.8.8.8")).willReturn(CallResult.makeCallResult(true, ResultEnum.SUCCESS, ipModel, null));
+    }
+
+    @Test
+    public void testQuery() throws Exception {
 
         CallResult<IpModel> ipModelCallResult = this.demoService.queryByIp("8.8.8.8");
 
@@ -43,7 +54,7 @@ public class DemoControllerTestByMock2 {
 
 
         Assertions.assertThat(ipModelCallResult).hasFieldOrPropertyWithValue("success", true);
-        Assertions.assertThat(ipModelCallResult.getBusinessResult()).hasFieldOrPropertyWithValue("city",  "testCity");
+        Assertions.assertThat(ipModelCallResult.getBusinessResult()).hasFieldOrPropertyWithValue("city", "testCity");
 
 
         ipModelCallResult = this.demoService.queryByIp("8.8.8.7");
@@ -51,13 +62,19 @@ public class DemoControllerTestByMock2 {
         System.out.println(JsonUtils.toJson(ipModelCallResult));
 
         Assertions.assertThat(ipModelCallResult).hasFieldOrPropertyWithValue("success", true);
-        Assertions.assertThat(ipModelCallResult.getBusinessResult()).hasFieldOrPropertyWithValue("city",  "testCity");
+        Assertions.assertThat(ipModelCallResult.getBusinessResult()).hasFieldOrPropertyWithValue("city", null);
 //        Assertions.assertThat(ipModelCallResult).is
 
     }
 
 
+    @Test
+    public void testQuery2() throws Exception {
 
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/demo/v1/ip?ip=8.8.8.8").accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("{\"code\":200,\"msg\":\"成功\",\"data\":{\"city\":\"testCity\",\"country\":\"testCountry\"}}"));
+
+    }
 
 
 }
